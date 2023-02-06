fbe8628d194c070b0c8a307f2178823165e3d0c8
PranJal597 <97882585+PranJal597@users.noreply.github.com>
2023-02-06 10:51:58 +0530

    Update linear_search.c

--- linear_search.c
+++ linear_search.c
@@ -1,7 +1,30 @@
 #include<stdio.h>
 int main()
 {
-  printf("hello world");
-  return 0;
+    int n,i,num,c=0;;
+    printf("Enter the array size: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter the search element: ");
+    scanf("%d", &num);
+    for(i=0;i<n;i++)
+    {
+        if(num==arr[i])
+        {
+            printf("Element found at position: %d", i+1);
+            c++;
+            break;
+        }
+    }
+    if(c==0)
+    {
+        printf("Number not found");
+    }
+    return 0;
 }
   
